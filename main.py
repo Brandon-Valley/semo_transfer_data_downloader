d541687e89a8ce88aa636bafaa790b0f323d674e
Brandon Valley <bavvh8@mst.edu>
2024-03-11 08:53:39 -0500

    waits until loaded

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 # Setup WebDriver (you need to download a driver for this, e.g., chromedriver)
 driver = webdriver.Chrome()
 
@@ -14,7 +20,11 @@ driver.get(url)
 link = driver.find_element(By.ID, "gdvInstWithEQ_btnCreditFromInstName_0")
 link.click()
 
-sleep(30)
+# sleep(30)
+
+# Wait until equiv. info loads
+wait = WebDriverWait(driver, 30)
+wait.until(EC.presence_of_element_located((By.ID, 'btnAddToMyEQList')))
 
 # Now the driver page source should have the new info loaded, you can save it or parse it as needed
 html_content = driver.page_source
